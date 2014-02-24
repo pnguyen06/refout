8da2fa41c1baeb90f4b0fb26d104ae2a0d44b597
Phil Nguyen <pnguyen06@gmail.com>
2014-02-23 22:30:46 -0500

    missing images

--- app/models/recruiter.rb
+++ app/models/recruiter.rb
@@ -1,5 +1,5 @@
 class Recruiter < ActiveRecord::Base
 	belongs_to :headhunter
 
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/recruiter_pic.jpg"
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/recruiters/missing_:style.jpg"
 end
