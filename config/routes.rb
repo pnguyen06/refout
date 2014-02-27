244b2fa4d074ae7ccdad4bec57fae051f44a76ed
Phil Nguyen <pnguyen06@gmail.com>
2014-02-26 22:11:59 -0500

    About and Contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Refout::Application.routes.draw do
   devise_for :users
   root "pages#home"
   get "about" => "pages#about"
+  get "contact" => "pages#contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
