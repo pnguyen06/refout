596823369169036239d5dfa3c8ced256e0715365
Phil Nguyen <pnguyen06@gmail.com>
2014-03-16 18:16:54 -0400

    job filter

--- app/models/headhunter.rb
+++ app/models/headhunter.rb
@@ -6,7 +6,7 @@ class Headhunter < ActiveRecord::Base
 	scope :PreMBA, -> {where(:CompanyPreMBA => true)}
 	scope :PostMBA, -> {where(:CompanyPostMBA => true)}
 	scope :Executive, -> {where(:CompanyExecutive => true)}
-	scope :hasJobs, -> {where("CompanyJobs != 'None'")}
+	scope :hasJobs, -> {where("CompanyJobs != ?", "None")}
 
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 end
